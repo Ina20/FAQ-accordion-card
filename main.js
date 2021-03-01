570f0be26406cf6fc5c560a80dcb8fb8ea680acc
Ina20 <inaszkola@gmail.com>
2021-03-01 20:10:19 +0100

    desktop design

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ function dropdown(id){
   document.getElementById("answer" + id).classList.toggle("show");
   if(document.getElementById("question" + id).style.fontWeight !== "700"){
     document.getElementById("question" + id).style.fontWeight = "700";
+    document.getElementById("arrow" + id).style.transform = "rotate(180deg)";
   }else {
     document.getElementById("question" + id).style.fontWeight = "400";
+    document.getElementById("arrow" + id).style.transform = "rotate(0deg)";
   }
 
 }
